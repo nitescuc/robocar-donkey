87c69186a24c01c0016ea8faab4e15d9b014fd80
Cristian Nitescu <nitescuc@hotmail.com>
2022-07-23 18:30:26 +0200

    fix camera bench feature

--- donkeycar/parts/camera_benchmark.py
+++ donkeycar/parts/camera_benchmark.py
@@ -5,6 +5,7 @@ import cv2
 class CameraBenchmark:
     def __init__(self):
         self.images = []
+        self.cont_images = []
         self.triggered = False
         self.processed = None
     
@@ -14,17 +15,23 @@ class CameraBenchmark:
             (10,10), 
             cv2.FONT_HERSHEY_SIMPLEX, 
             0.5,
-            (57, 255, 20),
+            (200, 200, 200),
             1)
         return image
     
     def join_images(self):
-        return np.reshape(np.array(self.images), (16*240, 16*120))
+        return np.vstack(self.cont_images + self.images)
 
     def run(self, image, trigger_value):
-        if trigger_value > 0.5 and len(self.images) < 16:
-            self.images.append(self.stamp_image(image, trigger_value))
+        image = self.stamp_image(image, trigger_value)
+        if trigger_value > 0.2:
+            if len(self.images) < 16:
+                self.images.append(image)
         else:
-            self.processed = self.join_images()
-            self.images = []
+            if len(self.images) >= 16:
+                self.processed = self.join_images()
+                self.images = []
+            if len(self.cont_images) >= 4:
+                del self.cont_images[0]
+            self.cont_images.append(image)
         return self.processed
